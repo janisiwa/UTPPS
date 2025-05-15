da3243a8afa5b4090cd1b00b4cb007bd642e7b50
Janis Wint <janis.wint@gmail.com>
2025-05-15 11:31:06 -0500

    create package and add to package info table

---
+++ Package.py
@@ -0,0 +1,30 @@
+import Hash_Table
+
+class Package:
+
+    def __init__(self, package_id: int, package_info_table:'Hash_Table'):
+        #assign the unique id to the new package
+        self.id= package_id
+
+        #check for duplicate unique id
+        if package_info_table.get(package_id):
+            raise ValueError(f"Package with id {package_id} already exists")
+
+        #open package file
+
+        #load in the package components
+        self.street_address='195 W Oakland Ave'
+        self.city='Salt Lake City'
+        self.state='UT'
+        self.zip_code='84115'
+        self.delivery_deadline_time='10:30:00'
+        self.delivery_deadline_date='05/15/2025'
+        self.weight_kg='21'
+        self.special_notes='can only be on truck 2'
+        self.delayed_delivery_time='10:30:00'
+        self.delivery_truck='Truck 2'
+        self.co_delivery='15,19'
+        self.new_address_needed='Yes'
+
+        #add pacakge to package info table
+        package_info_table.add(self.id, self)
--- main.py
+++ main.py
@@ -1,2 +1,8 @@
 import Hash_Table
+import Package
 
+package_info_table =  Hash_Table.Hash_Table()
+my_package =  Package.Package(5,package_info_table)
+package_info_table_result = package_info_table.get(5)
+package_info_table_results = package_info_table.get(5).street_address
+print(f"Packing info: {my_package.street_address} \n Package table info: {package_info_table_results}")
