6d44d0b30c752110ac9efa47bf1cd9e159784239
Janis Wint <janis.wint@gmail.com>
2025-05-16 18:12:08 -0500

    load address file as dict, distances as list

--- main.py
+++ main.py
@@ -15,11 +15,12 @@ def runUTPPS():
     data_service.make_packages(package_data, package_info_table)
 
     #load in distance data from file
-    distance_data = data_service.get_data('distance_file')
+    distance_data = list(data_service.get_data('distance_file'))
+    print(distance_data[3][2])
 
     #load in address data from file
-    address_data = data_service.get_data('address_file')
-
-
+    address_data_lines = list(data_service.get_data('address_file'))
+    address_data = {x: values for x, *values in address_data_lines}
+    print(address_data.get('3365 S 900 W|84119'))
 if __name__ == "__main__":
     runUTPPS()
