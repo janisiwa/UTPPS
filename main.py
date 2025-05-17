48b82bbb326bd8effe8b30f13ae3311ef349d223
Janis Wint <janis.wint@gmail.com>
2025-05-16 23:21:52 -0500

    reformatted pacakage data file and ready to load trucks

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ import Hash_Table
 from Services import DataServices
 from Truck import Truck
 
+
+def load_trucks(truck_list, distance_list, address_list, package_info_table):
+    pass
+
+
 def run_UTPPS():
     # store and retrieve package information
     package_info_table = Hash_Table.Hash_Table()
@@ -14,16 +19,20 @@ def run_UTPPS():
     distance_list = open_store_distances(data_service)
     address_list = open_store_addresses(data_service)
 
-    #create the trucks
+    #create the empty trucks
     truck_list = open_store_trucks(data_service)
+
+    load_trucks(truck_list,distance_list,address_list,package_info_table)  
 
 
 def open_store_trucks(data_service):
     truck_list = []
+    #read in the truck information from the config file
     number_of_trucks = int(data_service.get_config_info('truck_info','trucks_available').strip())
     package_max=int(data_service.get_config_info('truck_info','package_maximum'))
     driver_list = data_service.get_config_info('truck_info','truck_drivers_available').split(',')
 
+    #create the trucks
     for i in range(number_of_trucks):
         truck=Truck(i+1,package_max,driver_list[i])
         truck_list.append(truck)
